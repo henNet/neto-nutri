176d7fde9124bf5914103f47182988ca1e63b193
henNet <dmf.n123@gmail.com>
2025-06-16 13:47:51 -0300

    feat: projeto neto-nutri

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,25 @@
+"use client";
+import "./Form.css";
+
+export default function Form() {
+  function handleGetDiet(formData) {
+    console.log(formData.get("name"));
+    console.log(formData.get("weight"));
+  }
+
+  return (
+    <div>
+      <div className="form-container">
+        <form action={handleGetDiet}>
+          Nome: <input type="text" name="name" />
+          Peso: <input type="number" name="weight" />
+          <input type="submit" value="Gerar dieta" />
+        </form>
+      </div>
+
+      <div className="response-container">
+        <p>Resposta</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>NetoNutri</h1>
+      <hr />
+    </div>
+  );
+}
